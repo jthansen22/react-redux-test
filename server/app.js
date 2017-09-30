4a52c25c53cf2d81725146ded34e58eb92090ab9
jthansen22 <jthansen22@gmail.com>
2017-09-29 18:24:47 -0700

    Initial Webpack Setup & Render Main React Component

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+
+import webpackConfig from '../webpack.config.dev';
 
 // Express Init
 const app = express();
@@ -7,12 +11,14 @@ const app = express();
 // Server Init
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
 // Index Route
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server Started on Port: ${PORT}`);
-})
-
+});
